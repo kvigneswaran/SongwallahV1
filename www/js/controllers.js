0c87ef3341ea0fb2fb4bf6a6cf195da903276f72
bhaskargr <bhaskar.gandavabi@gmail.com>
2014-05-03 15:58:25 -0400

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -310,7 +310,7 @@ angular.module('swMobile.controllers', [])
 	    		if (!$rootScope.nowPlaying) {
 	    			Player.getPlayer().doc = $document[0];
 	    			Player.getPlayer().stop();
-	    			Player.getPlayer().play($scope.clip.url);
+	    			Player.getPlayer().play($scope.clip);
 	    		}
 	        }, 500);
     	};
